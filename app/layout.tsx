53035254bcae38ab42948fa97dcf9c6d7e7d256d
Jjingo Farouk <jjingofarouq@gmail.com>
2025-05-17 22:36:54 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import ClientLayout from '@/components/ClientLayout';
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
-      <body className="bg-amber-50 text-gray-800 min-h-screen">
+      <body>
         <ClientLayout>{children}</ClientLayout>
       </body>
     </html>
