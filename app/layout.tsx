3f5ecf0fdcc4cbfc846ba49b8959b2c5e3a6c128
jjingofarouk <jjingofaroukk@gmail.com>
2025-05-17 20:43:55 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,13 @@
 // app/layout.tsx
 import { ReactNode } from 'react';
-import Header from '@/components/Header';
-import { AuthProvider } from '@/lib/auth';
 import './globals.css';
+import ClientLayout from '@/components/ClientLayout';
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <body className="bg-gray-100 text-gray-900 min-h-screen">
-        <AuthProvider>
-          <Header />
-          <main className="container mx-auto p-4">{children}</main>
-        </AuthProvider>
+        <ClientLayout>{children}</ClientLayout>
       </body>
     </html>
   );
