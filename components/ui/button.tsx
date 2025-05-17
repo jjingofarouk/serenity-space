8c3466bdb3617cab42bb15a2069202f9b9db6306
Jjingo Farouk <jjingofarouq@gmail.com>
2025-05-17 23:01:30 +0300

    Update button.tsx

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -1,6 +1,7 @@
 // components/ui/button.tsx
 import * as React from 'react';
 import { cn } from '@/lib/utils';
+import styles from './button.module.css';
 
 const Button = React.forwardRef<
   HTMLButtonElement,
@@ -8,10 +9,7 @@ const Button = React.forwardRef<
 >(({ className, ...props }, ref) => {
   return (
     <button
-      className={cn(
-        'inline-flex items-center justify-center rounded-xl text-sm font-medium transition-all duration-200 ease-in-out focus:outline-none focus:ring-2 focus:ring-teal-500 focus:ring-offset-2 disabled:opacity-50 disabled:pointer-events-none bg-gray-200 text-gray-800 hover:bg-gray-300',
-        className
-      )}
+      className={cn(styles.button, className)}
       ref={ref}
       {...props}
     />
