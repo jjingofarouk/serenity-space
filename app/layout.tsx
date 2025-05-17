a7587869f615bc266afad2696607d7c614a31e05
Jjingo Farouk <jjingofarouq@gmail.com>
2025-05-18 01:58:15 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <ThemeProvider>
           <ClientLayout>
-            <Header />
+
             {children}
           </ClientLayout>
         </ThemeProvider>
