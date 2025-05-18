a7eb00b1382d6143e6f950048ee0cfc98167e6bb
Jjingo Farouk <jjingofarouq@gmail.com>
2025-05-18 05:10:52 +0300

    Update types.ts

--- lib/types.ts
+++ lib/types.ts
@@ -1,17 +1,17 @@
 // lib/types.ts
 import { Timestamp } from 'firebase/firestore';
 
-export interface Message {
-  id: string;
-  text: string;
-  userId: string;
-  createdAt: Timestamp | Date;
-}
-
 export interface Post {
   id: string;
   title: string;
   content: string;
+  userId: string;
+  createdAt: Timestamp | Date;
+}
+
+export interface Message {
+  id: string;
+  text: string;
   userId: string;
   createdAt: Timestamp | Date;
 }
